a000551067e57468b4b6c4e6ade9029606c6c7f7
Ruslan Shaiakhmetov <ruslan.shaiakhmetov@unibo.it>
2024-06-06 22:35:30 +0000

    feat: ds vs ph

--- main.py
+++ main.py
@@ -74,304 +74,6 @@ def plot_line_shadow(list_in: list, res: tuple, linetype: str, color, name: str)
         color=color
     )
 
-def pid_runnung(dict_in: dict):
-    
-    obj = pid_tracking.PID_Tracking(dict_in)
-    result = obj.trajectory_tracking()
-    rss = obj.rss
-    return rss
-
-def deepc_runnung(dict_in: dict):
-    
-    obj = deepc_tracking.DEEPC_Tracking(dict_in)
-    result = obj.trajectory_tracking()
-    rss = obj.rss
-    return rss
-
-def mpc_running(dict_in: dict):
-    obj = deepc_tracking.MPC_Tracking(dict_in)
-    result = obj.trajectory_tracking()
-    rss = obj.rss
-    return rss
-
-
-def pid_parallel_run_lis(list_in: list, var_param: str):
-        parameters = {
-            "heading_loop": [2.0, 0.0, 0.0],
-            "velocity_loop": [3.0, 0.0, 0.0],
-            "direction_loop": [0.1, 0.0, 0.0],
-            "distance_loop": [0.3, 0.0, 0.0],
-        }
-        param_list = []
-        for pac in list_in:
-            local_param = parameters.copy()
-            local_param[var_param] = pac
-            param_list.append(local_param)
-            
-        # Execution
-        pool = multiprocessing.Pool()
-        execution_result = pool.map(pid_runnung, param_list)
-        pool.close()
-        pool.join()
-            
-        return execution_result
-
-def pid_parallel_run_loose_lis(list_in: list, var_param: str):
-    parameters = {
-        "heading_loop": [2.0, 0.0, 0.0],
-        "velocity_loop": [3.0, 0.0, 0.0],
-        "direction_loop": [0.1, 0.0, 0.0],
-        "distance_loop": [0.1, 0.0, 0.0],
-        "pacejka_D": 1.0,
-    }
-    param_list = []
-    for pac in list_in:
-        local_param = parameters.copy()
-        local_param[var_param] = pac
-        param_list.append(local_param)
-        
-    # Execution
-    pool = multiprocessing.Pool()
-    execution_result = pool.map(pid_runnung, param_list)
-    pool.close()
-    pool.join()
-        
-    return execution_result
-
-def deepc_parallel_run_lis(list_in: list, var_param: str, N:int):
-    parameters = {
-        'lambda_g': 5.0*float(N)/100.0,
-        'lambda_y': [2e2] * 4,
-        'N': N,
-        'Q': [1, 1, 1, 100],
-        'R': [0.1, 0.1],
-        'prediction_horizon': 8,
-        'track_upper_bound': 100.0,
-    }
-    param_list = []
-    for pac in list_in:
-        local_param = parameters.copy()
-        local_param[var_param] = pac
-        for seed in range(30):
-            local_local_param = local_param.copy()
-            local_local_param["seed"] = seed
-            param_list.append(local_local_param)
-    
-    # Execution
-    pool = multiprocessing.Pool()
-    execution_result = pool.map(deepc_runnung, param_list)
-    pool.close()
-    pool.join()
-    
-    average_list = []
-    sigma_list = []
-    counter = 0
-    for _ in range(len(list_in)):
-        seed_list = []
-        for seed in range(30):
-            seed_list.append(execution_result[counter])
-            counter +=1
-        average = sum(seed_list)/len(seed_list)
-        average_list.append(average)
-        seed_average_nd = np.array(seed_list)
-        sigma = np.sqrt(sum(np.square(seed_average_nd-average))/seed_average_nd.size)
-        sigma_list.append(sigma)
-        
-    return average_list, sigma_list
-    
-def mpc_parallel_run_lis(list_in: list, var_param: str):
-    parameters = {
-        'Q': [1, 1, 1, 100],
-        'R': [0.1, 0.1],
-        'prediction_horizon': 8,
-    }
-    param_list = []
-    for pac in list_in:
-        local_param = parameters.copy()
-        local_param[var_param] = pac
-        param_list.append(local_param)
-        
-    # Execution
-    pool = multiprocessing.Pool()
-    execution_result = pool.map(deepc_runnung, param_list)
-    pool.close()
-    pool.join()
-        
-    return execution_result
-    
-def abu_dhabi_chart():
-    
-    def mpc_parallel_run_abu(list_in: list, var_param: str):
-        parameters = {
-            'track': "ABU_F2.csv",
-            'Q': [1, 1, 1, 100],
-            'R': [0.1, 0.1],
-            'prediction_horizon': 8,
-            'track_upper_bound': 100.0,
-        }
-        param_list = []
-        for pac in list_in:
-            local_param = parameters.copy()
-            local_param[var_param] = pac
-            param_list.append(local_param)
-            
-        # Execution
-        pool = multiprocessing.Pool()
-        execution_result = pool.map(deepc_runnung, param_list)
-        pool.close()
-        pool.join()
-            
-        return execution_result
-    
-    def pid_parallel_run_abu(list_in: list, var_param: str):
-        parameters = {
-            "heading_loop": [2.0, 0.0, 0.0],
-            "velocity_loop": [3.0, 0.0, 0.0],
-            "direction_loop": [0.1, 0.0, 0.0],
-            "distance_loop": [0.3, 0.0, 0.0],
-            "track": "ABU_F2.csv",
-            "pacejka_D": 1.0,
-            'track_upper_bound': 100.0,
-        }
-        param_list = []
-        for pac in list_in:
-            local_param = parameters.copy()
-            local_param[var_param] = pac
-            param_list.append(local_param)
-            
-        # Execution
-        pool = multiprocessing.Pool()
-        execution_result = pool.map(pid_runnung, param_list)
-        pool.close()
-        pool.join()
-            
-        return execution_result
-
-    def pid_parallel_run_loose_abu(list_in: list, var_param: str):
-        parameters = {
-            "heading_loop": [2.0, 0.0, 0.0],
-            "velocity_loop": [3.0, 0.0, 0.0],
-            "direction_loop": [0.1, 0.0, 0.0],
-            "distance_loop": [0.1, 0.0, 0.0],
-            "track": "ABU_F2.csv",
-            "pacejka_D": 1.0,
-            'track_upper_bound': 100.0,
-        }
-        param_list = []
-        for pac in list_in:
-            local_param = parameters.copy()
-            local_param[var_param] = pac
-            param_list.append(local_param)
-            
-        # Execution
-        pool = multiprocessing.Pool()
-        execution_result = pool.map(pid_runnung, param_list)
-        pool.close()
-        pool.join()
-            
-        return execution_result
-
-    def deepc_parallel_run_abu(list_in: list, var_param: str, N:int):
-        parameters = {
-            'lambda_g': 5.0*float(N)/100.0,
-            'lambda_y': [2e2] * 4,
-            'N': N,
-            'track': "ABU_F2.csv",
-            'Q': [1, 1, 1, 100],
-            'R': [0.1, 0.1],
-            'prediction_horizon': 8,
-            'track_upper_bound': 100.0,
-        }
-        param_list = []
-        for pac in list_in:
-            local_param = parameters.copy()
-            local_param[var_param] = pac
-            for seed in range(30):
-                local_local_param = local_param.copy()
-                local_local_param["seed"] = seed
-                param_list.append(local_local_param)
-        
-        # Execution
-        pool = multiprocessing.Pool()
-        execution_result = pool.map(deepc_runnung, param_list)
-        pool.close()
-        pool.join()
-        
-        average_list = []
-        sigma_list = []
-        counter = 0
-        for _ in range(len(list_in)):
-            seed_list = []
-            for seed in range(30):
-                seed_list.append(execution_result[counter])
-                counter +=1
-            average = sum(seed_list)/len(seed_list)
-            average_list.append(average)
-            seed_average_nd = np.array(seed_list)
-            sigma = np.sqrt(sum(np.square(seed_average_nd-average))/seed_average_nd.size)
-            sigma_list.append(sigma)
-            
-        return average_list, sigma_list
-    
-     # Execution of the experiments
-    
-    list_in = np.arange(1.0, 2.0, 0.1)
-    pid_res = pid_parallel_run_abu(list_in, "pacejka_D")
-    pid_res_loose = pid_parallel_run_loose_abu(list_in, "pacejka_D")
-    deepc_res50, deepc_sig_50 = deepc_parallel_run_abu(list_in, "pacejka_D", 50)
-    deepc_res100, deepc_sig_100 = deepc_parallel_run_abu(list_in, "pacejka_D", 100)
-    deepc_res200, deepc_sig_200 = deepc_parallel_run_abu(list_in, "pacejka_D", 200)
-    deepc_res400, deepc_sig_400 = deepc_parallel_run_abu(list_in, "pacejka_D", 400)
-    mpc_res = mpc_parallel_run_abu(list_in, "pacejka_D")
-    
-    # Experiment plotting
-    fig, ax = plt.subplots()
-    plt.plot(list_in, pid_res, label="PID agressive", linestyle='--', color='black')
-    plt.plot(list_in, pid_res_loose, label="PID loose", linestyle='-.', color='black')
-    plt.plot(list_in, mpc_res, label="MPC", linestyle='--', color='red')
-    plt.plot(list_in, deepc_res50, label="DeePC, N=50", color=viridis(float(0.25)))
-    plt.fill_between(
-        list_in, 
-        [deepc_res50[ogni] - deepc_sig_50[ogni] for ogni in range(len(list_in))], 
-        [deepc_res50[ogni] + deepc_sig_50[ogni] for ogni in range(len(list_in))], 
-        alpha=0.3, 
-        color=viridis(0.25)
-    )
-    plt.plot(list_in, deepc_res100, label="DeePC, N=100", color=viridis(float(0.5)))
-    plt.fill_between(
-        list_in, 
-        [deepc_res100[ogni] - deepc_sig_100[ogni] for ogni in range(len(list_in))], 
-        [deepc_res100[ogni] + deepc_sig_100[ogni] for ogni in range(len(list_in))], 
-        alpha=0.3, 
-        color=viridis(0.5)
-    )
-    plt.plot(list_in, deepc_res200, label="DeePC, N=200", color=viridis(float(0.75)))
-    plt.fill_between(
-        list_in, 
-        [deepc_res200[ogni] - deepc_sig_200[ogni] for ogni in range(len(list_in))], 
-        [deepc_res200[ogni] + deepc_sig_200[ogni] for ogni in range(len(list_in))], 
-        alpha=0.3, 
-        color=viridis(0.75)
-    )
-    plt.plot(list_in, deepc_res400, label="DeePC, N=400", color=viridis(float(1.0)))
-    plt.fill_between(
-        list_in, 
-        [deepc_res400[ogni] - deepc_sig_400[ogni] for ogni in range(len(list_in))], 
-        [deepc_res400[ogni] + deepc_sig_400[ogni] for ogni in range(len(list_in))], 
-        alpha=0.3, 
-        color=viridis(1.0)
-    )
-    plt.xlabel("Track peak attrition (Pacejka D)")
-    plt.ylabel("RSS of tracking deviation per step, m")
-    #ax.set_yscale('log')
-    ax.set_yscale('symlog', linthresh=1e-1)
-    #ax.set_xscale('log')
-    plt.legend()
-    plt.grid()
-    plt.title("Tires friction variation on Abu Dhabi circuit")
-    plt.savefig(f"img/Abu_Dhabi.pdf")
-    print(f"Look at the picture: img/Abu_Dhabi.pdf")
-    
 def nice_abu_dhabi_picture():
     # PID part
     parameters = {
@@ -432,7 +134,7 @@ def nice_abu_dhabi_picture():
  
 def abu_dhabi():
     
-    list_in = np.arange(1.0, 2.0, 0.1)
+    list_in = np.arange(0.8, 1.6, 0.01)
     # Experiment plotting
     fig, ax = plt.subplots()
     
@@ -480,8 +182,7 @@ def abu_dhabi():
     plt.title("Tires friction variation on Abu Dhabi circuit")
     plt.savefig(f"img/Abu_Dhabi_new.pdf")
     print(f"Look at the picture: img/Abu_Dhabi_new.pdf")
-    
-    
+       
 def circle_time():
     
     list_in = range(3000, 4500, 100)
@@ -531,12 +232,75 @@ def circle_time():
     plt.savefig(f"img/Circle_time_new.pdf")
     print(f"Look at the picture: img/Circle_time_new.pdf")
     
+def pid_optimization_abu_dhabi():
+    list_in = np.arange(0.8, 1.6, 0.01)
+    def cost_function(vec: list):
+        parameters = {
+            "heading_loop": [vec[0], 0.0, 0.0],
+            "velocity_loop": [vec[1], 0.0, 0.0],
+            "direction_loop": [vec[2], 0.0, 0.0],
+            "distance_loop": [vec[3], 0.0, 0.0],
+            'track': "ABU_F2.csv",
+        }
+        res, _ = run_parallel(parameters, list_in, "pacejka_D")
+        return sum(res)
+    
+    def gradient(vec: list):
+        base = cost_function(vec)
+        print("Base cost: "+str(base))
+        step = 0.01
+        result = []
+        for i in range(4):
+            local_vec = vec.copy()
+            local_vec[i] += step
+            local = cost_function(local_vec)
+            result.append((local-base)/step)
+        return result
+
+    alpha = 0.1
+    for iter in range(100):
+        vec = [1.0, 1.0, 0.1, 0.1]
+        g_vec = gradient(vec)
+        for i in range(4):
+            vec[i] -= g_vec[i] * alpha
+    print("Resultance vec" + str(vec))
+    return vec
+    
+def ph_vs_ds():
+    list_in = [2, 4, 6, 8, 10, 12, 16]
+    # Experiment plotting
+    fig, ax = plt.subplots()
+    N_list = [50, 100, 200, 400]
+    for i in range(4):
+        deepc_parameters = {
+            'Q': [1, 1, 1, 100],
+            'R': [0.1, 0.1],
+            'prediction_horizon': 8,
+            'lambda_g': 5.0*float(N_list[i])/100.0,
+            'lambda_y': [2e2] * 4,
+            'N': N_list[i],
+            "algorithm": "deepc",
+            "Lissajous_circle_time": 4500,
+        }
+        deepc = run_parallel(deepc_parameters, list_in, "prediction_horizon", True)
+        plot_line_shadow(list_in, deepc, '-', viridis(float(i/len(N_list))), "DeePC, N="+str(N_list[i]))
+    plt.xlabel("Lissajous total time, s")
+    plt.ylabel("RSS of tracking deviation per step, m")
+    #ax.set_yscale('log')
+    ax.set_yscale('symlog', linthresh=1e0)
+    #ax.set_xscale('log')
+    plt.legend()
+    plt.grid()
+    plt.title("Tracking error vs. trace difficulty")
+    plt.savefig(f"img/Circle_time_new.pdf")
+    print(f"Look at the picture: img/Circle_time_new.pdf")
+    
 if __name__ == "__main__":
     
-    #abu_dhabi_chart()
-    #nice_abu_dhabi_picture()
+    pid_optimization_abu_dhabi()
+    
     #circle_time()
-    abu_dhabi()
+    #abu_dhabi()
     exit()
     print('Hello world!')
     parameters = {
