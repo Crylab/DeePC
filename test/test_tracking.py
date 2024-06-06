863026937ef2859410dba3ddd6a1f14c866efde6
Ruslan Shaiakhmetov <ruslan.shaiakhmetov@unibo.it>
2024-06-06 15:31:21 +0200

    feat: mpc test

--- test/test_tracking.py
+++ test/test_tracking.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(1, 'source/')
 import pid_tracking 
 import deepc_tracking
+import mpc_tracking
 
 def test_always_passes():
     assert True
@@ -25,3 +26,14 @@ def test_deepc_tracking():
     obj.trajectory_tracking()
     rss = obj.get_rss()
     assert rss < 0.2
+    
+def test_mpc_tracking():
+    parameters = {
+        "Q": [1.0, 1.0, 10.0, 10.0],
+        "R": [0.1, 0.1],
+        "prediction_horizon": 8,
+    }
+    obj = mpc_tracking.MPC_Tracking(parameters)
+    obj.trajectory_tracking()
+    rss = obj.get_rss()
+    assert rss < 1.0
